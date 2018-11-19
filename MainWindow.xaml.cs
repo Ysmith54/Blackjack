4287568c21806da1c1bb93580e88603eb774f03a
Ysmith54 <44986938+Ysmith54@users.noreply.github.com>
2018-11-18 20:57:37 -0600

    Add files via upload
    I uploaded the updated files for the solution, its code, and the zip of the card images
    .

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Blackjack01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Start_Click(object sender, RoutedEventArgs e)
+        {
+            // Call window that contains actual game //
+            var win = new Game();
+            win.Show();
+
+        }
+    }
+}
